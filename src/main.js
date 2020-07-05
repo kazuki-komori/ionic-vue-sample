75070d8be70b980d40b1d7ab3efb2928d8e564fc
kazuki-komori <komokazu.nankai@gmail.com>
2020-07-05 09:05:45 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Ionic from '@ionic/vue'
+import '@ionic/core/css/core.css'
+import '@ionic/core/css/ionic.bundle.css'
+
+Vue.use(Ionic)
 
 Vue.config.productionTip = false
 
